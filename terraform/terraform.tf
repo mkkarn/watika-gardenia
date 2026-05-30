a8282770c0b3d1eac7610a99db6ab09df372a8c2
Manish karn <mkkarn1904@gmail.com>
2026-05-30 11:54:33 +0530

    Added terraform bucket on s3

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "watika-terraform-backend-bucket"
+    key    = "s3-backend"
+    region = "ap-south-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
